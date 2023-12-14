473f8b0a30e25ac812e17e83be5604fa465037c9
cardosofede <federico.cardoso.e@gmail.com>
2023-12-13 22:45:10 -0300

    (feat) add environment variables

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from routers import manage_docker, communicate_bots
 
+load_dotenv()
 app = FastAPI()
 
 app.include_router(manage_docker.router)
