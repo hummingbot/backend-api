16eeb3430270671d1ba98b43c4059559b183c36f
cardosofede <federico.cardoso.e@gmail.com>
2023-12-13 21:51:56 -0300

    (feat) add model for instance creation payload

---
+++ models.py
@@ -0,0 +1,10 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class HummingbotInstanceConfig(BaseModel):
+    instance_name: str
+    credentials_profile: str
+    image: str = "hummingbot/hummingbot:latest"
+    autostart_script: Optional[str] = None
