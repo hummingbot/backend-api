86784036f452d92601d765a928c7b5102fff5dad
cardosofede <federico.cardoso.e@gmail.com>
2023-12-21 18:09:28 -0300

    (feat) improve implementation with aps

--- routers/manage_broker_messages.py
+++ routers/manage_broker_messages.py
@@ -1,28 +1,36 @@
 import asyncio
 import threading
 import time
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from fastapi import APIRouter, BackgroundTasks
 
 from utils.bots_manager import BotsManager
 
+# Initialize the scheduler
+scheduler = AsyncIOScheduler()
 router = APIRouter(tags=["Manage Broker Messages"])
 bots_manager = BotsManager(broker_host='localhost', broker_port=1883, broker_username='admin', broker_password='password')
-bots_manager.update_active_bots()
 
 
-def periodic_task():
-    while True:
-        bots_manager.update_active_bots()
-        print("active bots:")
-        for bot, data in bots_manager.active_bots.items():
-            print(data['bot_name'])
-        time.sleep(10)
+def update_active_bots():
+    bots_manager.update_active_bots()
+    print("active bots:")
+    for bot, data in bots_manager.active_bots.items():
+        print(data['bot_name'])
 
 
 @router.on_event("startup")
-def startup_event():
-    task_thread = threading.Thread(target=periodic_task)
-    task_thread.daemon = True  # Ensures that the thread will exit when the main process does
-    task_thread.start()
+async def startup_event():
+    # Add the job to the scheduler
+    scheduler.add_job(update_active_bots, 'interval', seconds=10)
+    scheduler.start()
+
+
+@router.on_event("shutdown")
+async def shutdown_event():
+    # Shutdown the scheduler on application exit
+    scheduler.shutdown()
+
+
 
