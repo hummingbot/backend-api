fbaac68490756b44abff9925a1ee77e25102610c
cardosofede <federico.cardoso.e@gmail.com>
2024-04-12 19:51:28 -0300

    (feat) improve bots manager

--- utils/bots_manager.py
+++ utils/bots_manager.py
@@ -1,5 +1,9 @@
+import re
+from collections import defaultdict
+
 import docker
 from hbotrc import BotCommands
+from hbotrc.msgs import StatusCommandMessage
 
 
 class BotsManager:
@@ -39,6 +43,8 @@ class BotsManager:
                                                  username=self.broker_username, password=self.broker_password,
                                                  bot_id=bot)
                 }
+        for bot, data in self.active_bots.items():
+            data["status"] = self.get_bot_status(bot)
 
     # Interact with a specific bot
     def start_bot(self, bot_name, **kwargs):
@@ -59,8 +65,84 @@ class BotsManager:
 
     def get_bot_status(self, bot_name, **kwargs):
         if bot_name in self.active_bots:
-            return self.active_bots[bot_name]["broker_client"].status(**kwargs)
+            status: StatusCommandMessage = self.active_bots[bot_name]["broker_client"].status(**kwargs)
+            return self.parse_status_message(status.msg)
 
     def get_bot_history(self, bot_name, **kwargs):
         if bot_name in self.active_bots:
             return self.active_bots[bot_name]["broker_client"].history(**kwargs)
+
+    def parse_status_message(self, msg):
+        data = {}
+
+        # Extracting total balances
+        balance_pattern = r"Balances:\s*\n([\s\S]+?)\n\n"
+        balances = re.search(balance_pattern, msg)
+        balances_dict = {}
+        if balances:
+            balance_lines = balances.group(1).strip().split('\n')
+            for line in balance_lines:
+                parts = re.split(r'\s{2,}', line.strip())
+                if len(parts) >= 4:
+                    exchange, asset, total, available = parts[0], parts[1], parts[2], parts[3]
+                    if exchange not in balances_dict:
+                        balances_dict[exchange] = {}
+                    balances_dict[exchange][asset] = available
+        data['total_balances'] = balances_dict
+
+        # Extract controller specific data and assess running status
+        controller_data_dict = {}
+        controllers = re.finditer(
+            r"Controller:\s*([^\n]+)\s*\n\+[-+]+\+\n([\s\S]+?)\nRealized PNL \(Quote\): ([-\d.]+) \| Unrealized PNL \(Quote\): ([-\d.]+)--> Global PNL \(Quote\): ([-\d.]+) \| Global PNL \(%\): ([-\d.]+)%\nTotal Volume Traded: ([\d.]+)",
+            msg)
+        for controller in controllers:
+            controller_name = controller.group(1)
+            controller_data = controller.group(2)
+            realized_pnl_quote = controller.group(3)
+            unrealized_pnl_quote = controller.group(4)
+            global_pnl_quote = controller.group(5)
+            global_pnl_pct = controller.group(6)
+            total_volume_traded = controller.group(7)
+
+
+            controller_data_dict[controller_name] = {
+                'realized_pnl_quote': realized_pnl_quote,
+                'unrealized_pnl_quote': unrealized_pnl_quote,
+                'global_pnl_quote': global_pnl_quote,
+                'global_pnl_pct': global_pnl_pct,
+                'total_volume_traded': total_volume_traded,
+            }
+
+        data['controllers'] = controller_data_dict
+
+        # Determining running status from the controller data
+        running_status = self.determine_running_status(msg)
+        data['running_status'] = running_status
+        # Extract global performance summary
+        global_performance_pattern = (
+            r"Global Performance Summary:\nGlobal PNL \(Quote\): ([-\d.]+) \| Global PNL \(%\): ([-\d.]+)% \| "
+            r"Total Volume Traded \(Global\): ([\d.]+)"
+        )
+        global_performance = re.search(global_performance_pattern, msg)
+        if global_performance:
+            data['global_performance'] = {
+                'global_pnl_quote': global_performance.group(1),
+                'global_pnl_pct': global_performance.group(2),
+                'total_volume_traded': global_performance.group(3)
+            }
+
+        return data
+
+    @staticmethod
+    def determine_running_status(controller_data):
+        if "Market connectors are not ready" in controller_data:
+            return "starting"  # Bot is starting but not ready
+        elif "No strategy is currently running" in controller_data:
+            return "stopped"  # The bot is stopped
+        return "running"  # Default to running if none of the above
+
+    def get_all_bots_status(self):
+        all_bots_status = {}
+        for bot, bot_info in self.active_bots.items():
+            all_bots_status[bot] = {key: value for key, value in bot_info.items() if key != "broker_client"}
+        return all_bots_status
