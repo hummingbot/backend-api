ceda3839487e38fdeb4939ef7225bbb562acac09
cardosofede <federico.cardoso.e@gmail.com>
2024-05-15 17:15:37 -0400

    (feat) improve backtesting engine

--- utils/backtesting_engine.py
+++ utils/backtesting_engine.py
@@ -1,17 +1,13 @@
-from typing import List
+from typing import List, Dict
 import os
 import yaml
 import importlib
 import inspect
 
-import pandas as pd
 from hummingbot.strategy_v2.backtesting.backtesting_engine_base import BacktestingEngineBase
 from hummingbot.exceptions import InvalidController
 from hummingbot.strategy_v2.backtesting.backtesting_data_provider import BacktestingDataProvider
-from hummingbot.strategy_v2.controllers.controller_base import ControllerConfigBase
-from hummingbot.strategy_v2.controllers.market_making_controller_base import MarketMakingControllerConfigBase
-from hummingbot.strategy_v2.controllers.directional_trading_controller_base import DirectionalTradingControllerConfigBase
-
+from hummingbot.strategy_v2.controllers import ControllerConfigBase, MarketMakingControllerConfigBase, DirectionalTradingControllerConfigBase
 from hummingbot.data_feed.candles_feed.data_types import CandlesConfig
 
 import config
@@ -19,22 +15,25 @@ import config
 
 class BacktestingEngine(BacktestingEngineBase):
 
-    def __init__(self):
-        super().__init__()
-        self.backtesting_resolution = None
-        self.backtesting_data_provider = None
-
     @classmethod
-    def load_controller_config(cls, config_path: str) -> ControllerConfigBase:
+    def load_controller_config(cls, config_path: str) -> Dict:
         full_path = os.path.join(config.CONTROLLERS_PATH, config_path)
         with open(full_path, 'r') as file:
             config_data = yaml.safe_load(file)
+        return config_data
 
+    @classmethod
+    def get_controller_config_instance_from_yml(cls, config_path: str) -> ControllerConfigBase:
+        config_data = cls.load_controller_config(config_path)
+        return cls.get_controller_config_instance_from_dict(config_data)
+
+    @classmethod
+    def get_controller_config_instance_from_dict(cls, config_data: Dict) -> ControllerConfigBase:
         controller_type = config_data.get('controller_type')
         controller_name = config_data.get('controller_name')
 
         if not controller_type or not controller_name:
-            raise ValueError(f"Missing controller_type or controller_name in {config_path}")
+            raise ValueError(f"Missing controller_type or controller_name")
 
         module_path = f"{config.CONTROLLERS_MODULE}.{controller_type}.{controller_name}"
         module = importlib.import_module(module_path)
