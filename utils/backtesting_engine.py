41144535b071321771c98613aa8a91f21f010a11
cardosofede <federico.cardoso.e@gmail.com>
2024-05-15 14:06:57 -0400

    (feat) add backtesting adapter to manage multiple configs

---
+++ utils/backtesting_engine.py
@@ -0,0 +1,104 @@
+from typing import List
+import os
+import yaml
+import importlib
+import inspect
+
+import pandas as pd
+from hummingbot.strategy_v2.backtesting.backtesting_engine_base import BacktestingEngineBase
+from hummingbot.exceptions import InvalidController
+from hummingbot.strategy_v2.backtesting.backtesting_data_provider import BacktestingDataProvider
+from hummingbot.strategy_v2.controllers.controller_base import ControllerConfigBase
+from hummingbot.strategy_v2.controllers.market_making_controller_base import MarketMakingControllerConfigBase
+from hummingbot.strategy_v2.controllers.directional_trading_controller_base import DirectionalTradingControllerConfigBase
+
+from hummingbot.data_feed.candles_feed.data_types import CandlesConfig
+
+import config
+
+
+class BacktestingEngine(BacktestingEngineBase):
+
+    def __init__(self):
+        super().__init__()
+        self.backtesting_resolution = None
+        self.backtesting_data_provider = None
+
+    @classmethod
+    def load_controller_config(cls, config_path: str) -> ControllerConfigBase:
+        full_path = os.path.join(config.CONTROLLERS_PATH, config_path)
+        with open(full_path, 'r') as file:
+            config_data = yaml.safe_load(file)
+
+        controller_type = config_data.get('controller_type')
+        controller_name = config_data.get('controller_name')
+
+        if not controller_type or not controller_name:
+            raise ValueError(f"Missing controller_type or controller_name in {config_path}")
+
+        module_path = f"{config.CONTROLLERS_MODULE}.{controller_type}.{controller_name}"
+        module = importlib.import_module(module_path)
+
+        config_class = next((member for member_name, member in inspect.getmembers(module)
+                             if inspect.isclass(member) and member not in [ControllerConfigBase,
+                                                                           MarketMakingControllerConfigBase,
+                                                                           DirectionalTradingControllerConfigBase]
+                             and (issubclass(member, ControllerConfigBase))), None)
+        if not config_class:
+            raise InvalidController(f"No configuration class found in the module {controller_name}.")
+
+        return config_class(**config_data)
+
+    async def run_backtesting_configs(self,
+                                      controller_configs: List[ControllerConfigBase],
+                                      start: int, end: int,
+                                      backtesting_resolution: str = "1m",
+                                      trade_cost=0.0006):
+        # Load historical candles
+        self.reset_backtesting_data_provider(start, end, backtesting_resolution)
+        self.initialize_controllers(controller_configs)
+        await self.initialize_backtesting_data_provider()
+        results = await self.run_simulations(trade_cost=trade_cost)
+        return results
+
+    async def run_simulations(self, trade_cost=0.0006):
+        results = []
+        for controller in self.controllers:
+            result = await self.run_simulation(controller, trade_cost)
+            results.append(result)
+        return results
+
+
+    async def run_simulation(self, controller, trade_cost):
+        await controller.update_processed_data()
+        executors_info = self.simulate_execution(trade_cost=trade_cost)
+        results = self.summarize_results(executors_info)
+        return {
+            "executors": executors_info,
+            "results": results,
+            "processed_data": controller.processed_data,
+            "config": controller.config
+        }
+
+    async def initialize_backtesting_data_provider(self):
+        for controller in self.controllers:
+            backtesting_config = CandlesConfig(
+                connector=controller.config.connector_name,
+                trading_pair=controller.config.trading_pair,
+                interval=self.backtesting_resolution
+            )
+            await controller.market_data_provider.initialize_candles_feed(backtesting_config)
+            for config in controller.config.candles_config:
+                await controller.market_data_provider.initialize_candles_feed(config)
+
+    def initialize_controllers(self, controller_configs: List[ControllerConfigBase]):
+        self.controllers = []
+        for controller_config in controller_configs:
+            controller_class = controller_config.get_controller_class()
+            controllers = controller_class(config=controller_config, market_data_provider=backtesting_data_provider,
+                                           actions_queue=None)
+            self.controllers.append(controllers)
+
+    def reset_backtesting_data_provider(self, start: int, end: int, backtesting_resolution: str):
+        self.backtesting_resolution = backtesting_resolution
+        self.backtesting_data_provider = BacktestingDataProvider(connectors={}, start_time=start, end_time=end)
