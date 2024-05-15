16affc2dbfd3f2081fecf356eac6c130b982271e
cardosofede <federico.cardoso.e@gmail.com>
2024-05-15 14:13:06 -0400

    (feat) adapt to new library version

--- routers/manage_market_data.py
+++ routers/manage_market_data.py
@@ -24,7 +24,7 @@ async def get_candles(candles_config: CandlesConfig):
     try:
         candles = candles_factory.get_candle(candles_config)
         candles.start()
-        while not candles.is_ready:
+        while not candles.  ready:
             await asyncio.sleep(1)
         df = candles.candles_df
         candles.stop()
