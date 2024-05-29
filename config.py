168ca8bced9172fc2d4539cb6380a9a7728965bf
cardosofede <federico.cardoso.e@gmail.com>
2024-05-29 01:45:12 -0500

    (feat) add password verification in config

--- config.py
+++ config.py
@@ -6,7 +6,9 @@ load_dotenv()
 
 CONTROLLERS_PATH = "bots/conf/controllers"
 CONTROLLERS_MODULE = "bots.controllers"
+CONFIG_PASSWORD = os.getenv("CONFIG_PASSWORD", "a")
 BROKER_HOST = os.getenv("BROKER_HOST", "localhost")
 BROKER_PORT = int(os.getenv("BROKER_PORT", 1883))
 BROKER_USERNAME = os.getenv("BROKER_USERNAME", "admin")
 BROKER_PASSWORD = os.getenv("BROKER_PASSWORD", "password")
+PASSWORD_VERIFICATION_PATH = "bots/credentials/master_account/.password_verification"
