e4bc03db3d469149e6595843d2a0e8791d6ff7fe
cardosofede <federico.cardoso.e@gmail.com>
2024-06-24 20:39:53 +0200

    (feat) improve accounts state dump process

--- services/accounts_service.py
+++ services/accounts_service.py
@@ -77,24 +77,30 @@ class AccountsService:
         :return:
         """
         while True:
-            now = datetime.now()
-            next_log_time = (now + timedelta(minutes=self.account_history_dump_interval)).replace(second=0, microsecond=0)
-            next_log_time = next_log_time - timedelta(minutes=next_log_time.minute % self.account_history_dump_interval)
-            sleep_duration = (next_log_time - now).total_seconds()
-            await asyncio.sleep(sleep_duration)
             try:
                 await self.dump_account_state()
             except Exception as e:
                 logging.error(f"Error dumping account state: {e}")
+            finally:
+                now = datetime.now()
+                next_log_time = (now + timedelta(minutes=self.account_history_dump_interval)).replace(second=0,
+                                                                                                      microsecond=0)
+                next_log_time = next_log_time - timedelta(
+                    minutes=next_log_time.minute % self.account_history_dump_interval)
+                sleep_duration = (next_log_time - now).total_seconds()
+                await asyncio.sleep(sleep_duration)
 
     async def dump_account_state(self):
         """
-        Dump the current account state to a JSON file.
+        Dump the current account state to a JSON file. Create it if the file not exists.
         :return:
         """
         timestamp = datetime.now().isoformat()
         state_to_dump = {"timestamp": timestamp, "state": self.accounts_state}
-        file_system.append_to_file("data", self.history_file, json.dumps(state_to_dump) + "\n")
+        if not file_system.path_exists(path=f"data/{self.history_file}"):
+            file_system.add_file(directory="data", file_name=self.history_file, content=json.dumps(state_to_dump) + "\n")
+        else:
+            file_system.append_to_file(directory="data", file_name=self.history_file, content=json.dumps(state_to_dump) + "\n")
 
     def load_account_state_history(self):
         """
