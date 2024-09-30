228481772e20738de8a4b6a6fa795d60bfdefcc5
Yusuf Taiwo Hassan <teehazzan@gmail.com>
2024-09-30 06:43:31 +0100

    Added JWT

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+import os
+
 
 import routers.instances
 
 load_dotenv()
 app = FastAPI()
+
+os.environ['FASTAPI_WALLETAUTH_APP'] = 'hummingbot_instance_manager'
 
 app.include_router(routers.instances.router)
