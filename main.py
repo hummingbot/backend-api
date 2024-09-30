1def9f4b984ebdfd6d931aecb2577e46aefa51d4
Mike Hukiewitz <70762838+MHHukiewitz@users.noreply.github.com>
2024-09-30 08:10:14 +0200

    rename app name to robotter-ai

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ import routers.instances
 load_dotenv()
 app = FastAPI()
 
-os.environ['FASTAPI_WALLETAUTH_APP'] = 'hummingbot_instance_manager'
+os.environ['FASTAPI_WALLETAUTH_APP'] = 'robotter-ai'
 
 app.include_router(routers.instances.router)
