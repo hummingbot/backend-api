1bfc9d16f4bc9a5b3ef42b400ff19d29e70c9d02
cardosofede <federico.cardoso.e@gmail.com>
2024-10-13 18:37:44 -0300

    (feat) add logfire config

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import logfire
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
 from routers import manage_accounts, manage_backtesting, manage_broker_messages, manage_docker, manage_files, manage_market_data
 
 load_dotenv()
+
 app = FastAPI()
+logfire.configure()
+logfire.instrument_fastapi(app)
 
 app.include_router(manage_docker.router)
 app.include_router(manage_broker_messages.router)
