2dbaa1710b20c3339972a2400778ccf8efe87fa0
drupman <tomasgaudino@hotmail.com>
2024-11-14 22:42:53 -0300

    (feat) add lost lines from bad github management

--- routers/manage_databases.py
+++ routers/manage_databases.py
@@ -1,8 +1,11 @@
 import json
+import time
 
 from typing import List, Dict, Any
 
-from utils.etl_databases import HummingbotDatabase
+import pandas as pd
+
+from utils.etl_databases import HummingbotDatabase, ETLPerformance
 from fastapi import APIRouter
 
 from utils.file_system import FileSystemUtil
@@ -28,14 +31,15 @@ async def read_databases(db_paths: List[str] = None):
                 "healthy": db.status["general_status"],
                 "status": db.status,
                 "tables": {
-                    "order": json.dumps(db.get_orders().to_dict()),
+                    "orders": json.dumps(db.get_orders().to_dict()),
                     "trade_fill": json.dumps(db.get_trade_fills().to_dict()),
-                    "executor": json.dumps(db.get_executors_data().to_dict()),
-                    "order_status": json.dumps(db.get_order_status().to_dict())
+                    "executors": json.dumps(db.get_executors_data().to_dict()),
+                    "order_status": json.dumps(db.get_order_status().to_dict()),
+                    "controllers": json.dumps(db.get_controllers_data().to_dict())
                 }
             }
         except Exception as e:
-            print(f"Error reading database: {str(e)}")
+            print(f"Error reading database {db_path}: {str(e)}")
             db_content = {
                 "db_name": "",
                 "db_path": db_path,
@@ -45,3 +49,52 @@ async def read_databases(db_paths: List[str] = None):
             }
         dbs.append(db_content)
     return dbs
+
+
+@router.post("/create-checkpoint", response_model=Dict[str, Any])
+async def create_checkpoint(db_paths: List[str]):
+    try:
+        dbs = await read_databases(db_paths)
+
+        healthy_dbs = [db for db in dbs if db["healthy"]]
+
+        table_names = ["trade_fill", "orders", "order_status", "executors", "controllers"]
+        tables_dict = {name: pd.DataFrame() for name in table_names}
+
+        for db in healthy_dbs:
+            for table_name in table_names:
+                new_data = pd.DataFrame(json.loads(db["tables"][table_name]))
+                new_data["db_path"] = db["db_path"]
+                new_data["db_name"] = db["db_name"]
+                tables_dict[table_name] = pd.concat([tables_dict[table_name], new_data])
+
+        etl = ETLPerformance(db_path=f"bots/data/checkpoint_{str(int(time.time()))}.sqlite")
+        etl.create_tables()
+        etl.insert_data(tables_dict)
+        return {"message": "Checkpoint created successfully."}
+    except Exception as e:
+        return {"message": f"Error: {str(e)}"}
+
+
+@router.post("/list-checkpoints", response_model=List[str])
+async def list_checkpoints(full_path: bool):
+    return file_system.list_checkpoints(full_path)
+
+
+@router.post("/load-checkpoint")
+async def load_checkpoint(checkpoint_path: str):
+    try:
+        etl = ETLPerformance(checkpoint_path)
+        executor = etl.load_executors()
+        order = etl.load_orders()
+        trade_fill = etl.load_trade_fill()
+        controllers = etl.load_controllers()
+        checkpoint_data = {
+            "executors": json.dumps(executor.to_dict()),
+            "orders": json.dumps(order.to_dict()),
+            "trade_fill": json.dumps(trade_fill.to_dict()),
+            "controllers": json.dumps(controllers.to_dict())
+        }
+        return checkpoint_data
+    except Exception as e:
+        return {"message": f"Error: {str(e)}"}
--- routers/manage_performance.py
+++ routers/manage_performance.py
@@ -1,32 +1,26 @@
-from decimal import Decimal
-from typing import List
+from fastapi import APIRouter
+from typing import Any, Dict
 
-import json
-from hummingbot.strategy_v2.models.executors_info import ExecutorInfo
 from hummingbot.strategy_v2.backtesting.backtesting_engine_base import BacktestingEngineBase
 
-from routers.manage_backtesting import BACKTESTING_ENGINES
-
-from fastapi import APIRouter
+from utils.etl_databases import PerformanceDataSource
 
 router = APIRouter(tags=["Market Performance"])
 
 
 @router.post("/get-performance-results")
-async def get_performance_results(executors_and_controller_type: dict):
-    executors = executors_and_controller_type.get("executors")
-    controller_type = executors_and_controller_type.get("controller_type")
+async def get_performance_results(payload: Dict[str, Any]):
+    executors = payload.get("executors")
+    data_source = PerformanceDataSource(executors)
     performance_results = {}
     try:
-        for executor in executors:
-            if isinstance(executor["custom_info"], str):
-                executor["custom_info"] = json.loads(executor["custom_info"])
-        parsed_executors = [ExecutorInfo(**executor) for executor in executors]
-        backtesting_engine = BACKTESTING_ENGINES[controller_type]
-        performance_results["results"] = backtesting_engine.summarize_results(parsed_executors)
+        backtesting_engine = BacktestingEngineBase()
+        executor_info_list = data_source.executor_info_list
+        performance_results["results"] = backtesting_engine.summarize_results(executor_info_list    )
         results = performance_results["results"]
         results["sharpe_ratio"] = results["sharpe_ratio"] if results["sharpe_ratio"] is not None else 0
         return {
+            "executors": executors,
             "results": performance_results["results"],
         }
 
