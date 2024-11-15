23c028685a3508cc0f85fc203de3c0b0865d8e3d
drupman <tomasgaudino@hotmail.com>
2024-11-14 22:43:04 -0300

    (feat) add debug mode

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ security = HTTPBasic()
 
 username = os.getenv("USERNAME", "admin")
 password = os.getenv("PASSWORD", "admin")
+debug_mode = os.getenv("DEBUG_MODE", False)
 
 app = FastAPI()
 
@@ -31,7 +32,7 @@ def auth_user(
     is_correct_password = secrets.compare_digest(
         current_password_bytes, correct_password_bytes
     )
-    if not (is_correct_username and is_correct_password):
+    if not (is_correct_username and is_correct_password) and not debug_mode:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
